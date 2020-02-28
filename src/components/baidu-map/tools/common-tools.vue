f169e57b9ce9977b2d378ffbcdce2b940d2ef107
lihefei <lihefei@cnostar.com>
2020-02-28 12:03:25 +0800

    新增地图常用工具组件

---
+++ src/components/baidu-map/tools/common-tools.vue
@@ -0,0 +1,129 @@
+<template>
+   <bm-tool-wrapper :map="map" :BMap="BMap" >
+       <div class="common-tools">
+            <span 
+                v-for="type in toolTypes"
+                :class="{active: mapConfig[type].visible}"
+                class="tool-item"
+                @click="switchTypeClick(type)"
+            ><i :class="`iconfont icon-${mapConfig[type].icon}`"></i>{{mapConfig[type].title}}</span>
+       </div>
+       <bm-traffic v-if="mapConfig.traffic.visible"/>
+   </bm-tool-wrapper>
+</template>
+
+<script>
+import BaiduToolWrapper from './tool-wrapper'; //工具包装器
+import BuiduTraffic from '../layers/traffic'; //路况叠加层
+export default {
+    name: '',
+    components: {
+        'bm-tool-wrapper': BaiduToolWrapper,
+        'bm-traffic': BuiduTraffic
+    },
+    
+    props: {
+        map: Object,
+        BMap: Object,
+        toolTypes: {
+            type: Array,
+            default() {
+                return ['traffic', 'mapType'];
+            }
+        },
+        traffic: {
+            type: Boolean,
+            default: false
+        },
+        mapType: {
+            type: Boolean,
+            default: false
+        },
+    },
+    data() {
+        return {
+            mapConfig: {
+                map: null,
+                BMap: null,
+                traffic: {
+                    icon: 'traffic',
+                    title: '路况',
+                    visible: false
+                },
+                mapType: {
+                    icon: 'layer',
+                    title: '地图',
+                    visible: false
+                }
+            },
+        }
+    },
+    watch: {
+        map(map) {
+            debugger
+            this.mapConfig.map = map;
+        },
+        BMap(BMap) {
+            this.mapConfig.BMap = BMap;
+        },
+        traffic(val) {
+            this.mapConfig.traffic.visible = val;
+        },
+        mapType(val) {
+            this.mapConfig.mapType.visible = val;
+        },
+        'mapConfig.mapType.visible': function(val) {
+            let mapType = val ? BMAP_HYBRID_MAP : BMAP_NORMAL_MAP;
+            this.map.setMapType(mapType);
+        }
+    },
+    mounted() {
+        this.mapConfig.traffic.visible = this.traffic;
+        this.mapConfig.mapType.visible = this.mapType;
+    },
+    methods: {
+        /**
+         * 切换工具状态
+         * @param {Number} type 工具类型
+         */
+        switchTypeClick(type) {
+            this.mapConfig[type].visible = !this.mapConfig[type].visible;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.common-tools {
+    .tool-item {
+        font-size: 14px;
+        background: #fff;
+        border: none;
+        padding: 0 15px;
+        color: #606266;
+        cursor: pointer;
+        & > i {
+            margin-right: 5px;
+        }
+    }
+
+     .tool-item +  .tool-item {
+         position: relative;
+         &::before {
+             content: '';
+             position: absolute;
+             left: -1px;
+             top: 50%;
+             height: 80%;
+             border-left: 1px solid #ddd; 
+             transform: translateY(-50%);
+
+         }
+     }
+    
+    .active {
+        color: #409eff;
+    }
+}
+</style>
---
+++ src/views/tools/common-tools.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="wapper">
+        <baidu-map
+            ref="BDMap"
+            ak="S7gxefmASouTllUyjE71hWV3"
+            :center="mapConfig.center"
+            :zoom="mapConfig.zoom"
+            @ready="mapReady"
+        >
+            <bm-control anchor="BMAP_ANCHOR_TOP_RIGHT"  >
+                <bm-common-tools  
+                v-if="map" 
+                :map="map" 
+                :BMap="BMap" 
+                :traffic="true"
+            />
+            </bm-control>
+        </baidu-map>
+    </div>
+</template>
+<script>
+import BaiduMap from '@/components/baidu-map/map/map'; //地图
+import BuiduControl from '@/components/baidu-map/controls/control'; //自定义控制层
+import BuiduCommonTools from '@/components/baidu-map/tools/common-tools'; //常用工具
+
+export default {
+    name: '',
+    components: {
+        'baidu-map': BaiduMap,
+        'bm-control': BuiduControl,
+        'bm-common-tools': BuiduCommonTools
+    },
+
+    data() {
+        return {
+            map: null,
+            BMap: null,
+            mapConfig: {
+                center: { lng: 108.946267, lat: 34.349519 },
+                zoom: 7,
+            },
+           
+        };
+    },
+    watch: {},
+    
+
+    methods: {
+        mapReady({ BMap, map }) {
+            this.map = map;
+            this.BMap = BMap;
+            
+
+            console.log(BMap, map);
+        },
+        
+    }
+};
+</script>
+<style lang="scss" scoped>
+.wapper {
+    height: 100%;
+}
+</style>
